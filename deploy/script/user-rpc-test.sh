0b6f439635b8ca61229ab1b1e0c6d3e106157150
zhisongwu <songsongsongggg@gmail.com>
2025-03-01 00:41:22 +0800

    搭建部署脚本-修复问题，注意代理和docker网络

--- deploy/script/user-rpc-test.sh
+++ deploy/script/user-rpc-test.sh
@@ -4,15 +4,20 @@ tag='latest'
 
 container_name="easy-chat-user-rpc-test"
 
-docker stop ${container_name}
+network_name='easy-chat_easy-chat'
 
-docker rm ${container_name}
+# 停止并清理旧容器
+docker stop ${container_name} || true
+docker rm ${container_name} || true
+docker rmi ${reso_addr}:${tag} || true
 
-docker rmi ${reso_addr}:${tag}
-
+# 拉取新镜像
 docker pull ${reso_addr}:${tag}
+
+# 创建网络（若不存在）
+docker network inspect ${network_name} >/dev/null 2>&1 || docker network create ${network_name}
 
 
 # 如果需要指定配置文件的
 # docker run -p 10001:8080 --network imooc_easy-im -v /easy-im/config/user-rpc:/user/conf/ --name=${container_name} -d ${reso_addr}:${tag}
-docker run -p 10000:10000  --name=${container_name} -d ${reso_addr}:${tag}
+docker run -p 10000:10000 --network ${network_name} --name=${container_name} -d ${reso_addr}:${tag}
